53ed5955b4d2f6a70072dc773c9460e90e7a8b6b
sahapap <sahapap_panya558@outlook.co.th>
2025-07-19 16:17:25 +0700

    Install CORS and morgan.

--- Doze-Check-API/src/app.ts
+++ Doze-Check-API/src/app.ts
@@ -3,10 +3,17 @@ import exampleRouter from './routes/example.route';
 import stopBangRouter from './routes/stopBang.route'
 import epworthRouter from './routes/epworth.route'
 import accidentHistoryRouter from './routes/accidentHistory.route'
+import morgan from 'morgan';
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors())
+
+app.use(morgan('dev'));
+
 app.use('/api/example', exampleRouter);
 
 app.use('/api/stop-bang', stopBangRouter);
