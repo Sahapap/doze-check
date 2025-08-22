7ebc9f9749467980064a2663811738e57e5f3910
sahapap <sahapap_panya558@outlook.co.th>
2025-08-22 23:08:44 +0700

    Update firebase file.

--- Doze-Check-API/src/firebase/firebase.ts
+++ Doze-Check-API/src/firebase/firebase.ts
@@ -1,6 +1,9 @@
 import admin from 'firebase-admin'
 import 'dotenv/config'
-var serviceAccount = require("./doze-check-firebase-adminsdk-fbsvc-e191e0acaa.json")
+var serviceAccount =
+    process.env.NODE_ENV === "production" ?
+    JSON.parse(process.env.FIREBASE_CONFIG ?? "") :
+    require("./doze-check-firebase-adminsdk-fbsvc-e191e0acaa.json")
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
