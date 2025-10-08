525797d61065ea89833414f3617e6444eab40869
sahapap <sahapap_panya558@outlook.co.th>
2025-10-08 21:08:10 +0700

    Add /POST submit api.

--- Doze-Check-API/src/routes/submitForm.route.ts
+++ Doze-Check-API/src/routes/submitForm.route.ts
@@ -3,7 +3,14 @@ import { submitAssessment } from '../controllers/submitForm.controller'
 
 const router = Router();
 
+router.post('/', async(req, res) => {
+    console.log('call /POST submit')
+    const result = await submitAssessment(req.body)
+    res.json(result);
+});
+
 router.put('/', async(req, res) => {
+    console.log('call /PUT submit')
     const result = await submitAssessment(req.body)
     res.json(result);
 });
